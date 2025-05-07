7077965c989301d131fe9c901b68209c50594bdf
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-05-07 14:56:55 +0200

    Update const.py

--- custom_components/mailcow_ha_custom/const.py
+++ custom_components/mailcow_ha_custom/const.py
@@ -2,3 +2,5 @@
 DOMAIN = "mailcow_ha_custom"
 CONF_API_KEY = "api_key"
 CONF_BASE_URL = "base_url"
+IS_NOT_CHECK_START_HOUR = "is_not_check_start_hour"
+IS_NOT_CHECK_END_HOUR = "is_not_check_end_hour"
