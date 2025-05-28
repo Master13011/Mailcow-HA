3e1df088f9d3cd567a6935222386054f51566bed
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-05-28 14:23:04 +0200

    Update __init__.py

--- custom_components/mailcow_ha_custom/__init__.py
+++ custom_components/mailcow_ha_custom/__init__.py
@@ -9,7 +9,7 @@ from .api import MailcowAPI
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the Mailcow component."""
     hass.data.setdefault(DOMAIN, {})
     return True
@@ -18,12 +18,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Mailcow from a config entry."""
     session = async_get_clientsession(hass)
     api = MailcowAPI(entry.data, session)
-
     coordinator = MailcowCoordinator(hass, api, entry)
     await coordinator.async_config_entry_first_refresh()
-
     hass.data[DOMAIN][entry.entry_id] = coordinator
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
@@ -31,7 +28,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN].pop(entry.entry_id, None)
     return unload_ok
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
