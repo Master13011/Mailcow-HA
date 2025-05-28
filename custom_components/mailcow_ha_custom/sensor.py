0b2b7c061de6df3050c768754093a2103c18f71d
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-05-28 15:09:12 +0200

    Update sensor.py

--- custom_components/mailcow_ha_custom/sensor.py
+++ custom_components/mailcow_ha_custom/sensor.py
@@ -15,13 +15,14 @@ def extract_domain(url: str) -> str:
 class MailcowSensor(CoordinatorEntity, SensorEntity):
     def __init__(self, coordinator, name: str, key: str, icon: str):
         super().__init__(coordinator)
-        domain = extract_domain(coordinator._base_url)
-        self._attr_name = f"{domain}_mailcow_{key}"
+        self._attr_name = name
         self._attr_icon = icon
         self._key = key
         self._base_url = coordinator._base_url
         self._entry_id = coordinator.entry_id
-        self._attr_unique_id = f"mailcow_{key}_{domain}_{self._entry_id}"
+        self._attr_unique_id = (
+            f"mailcow_{key}_{sanitize_url(coordinator._base_url)}_{coordinator.entry_id}"
+        )
         self._attr_state_class = None
 
     @property
