aed0d9e425c0d731318961a0850f76a41d28f4b0
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-06-03 12:24:06 +0200

    Update config_flow.py

--- custom_components/mailcow_ha_custom/config_flow.py
+++ custom_components/mailcow_ha_custom/config_flow.py
@@ -85,11 +85,14 @@ class MailcowConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return MailcowOptionsFlowHandler()
+        return MailcowOptionsFlowHandler(config_entry)
 
 
 class MailcowOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle options flow for Mailcow."""
+
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
