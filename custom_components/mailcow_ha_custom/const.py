6fb1b0564153699264bc32c9385301d56756b0cc
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-06-03 09:42:03 +0200

    Update const.py

--- custom_components/mailcow_ha_custom/const.py
+++ custom_components/mailcow_ha_custom/const.py
@@ -5,5 +5,7 @@ CONF_API_KEY = "api_key"
 CONF_BASE_URL = "base_url"
 CONF_DISABLE_CHECK_AT_NIGHT = "disable_check_at_night"
 CONF_SCAN_INTERVAL = "scan_interval"
+CONF_NIGHT_START_HOUR = "night_start_hour"
+CONF_NIGHT_END_HOUR = "night_end_hour"
 
 PLATFORMS = ["sensor", "update"]
