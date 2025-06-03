76e5c6398a5c56a8855384c3158789598f00ced1
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-06-03 15:17:27 +0200

    Update exceptions.py

--- custom_components/mailcow_ha_custom/exceptions.py
+++ custom_components/mailcow_ha_custom/exceptions.py
@@ -2,6 +2,12 @@
 
 class MailcowAPIError(Exception):
     """Erreur générique liée à l'API Mailcow."""
+    def __init__(self, message=None):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message or self.__class__.__name__
 
 
 class MailcowAuthenticationError(MailcowAPIError):
