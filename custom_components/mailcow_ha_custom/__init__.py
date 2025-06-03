f29a8722ff6a65d1abc97d2d5575c69432279e91
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-06-03 14:06:51 +0200

    Update __init__.py

--- custom_components/mailcow_ha_custom/__init__.py
+++ custom_components/mailcow_ha_custom/__init__.py
@@ -3,6 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.config import ConfigType
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import aiohttp
 
 from .const import DOMAIN, PLATFORMS, CONF_SCAN_INTERVAL, CONF_DISABLE_CHECK_AT_NIGHT, CONF_BASE_URL
 from .coordinator import MailcowCoordinator
@@ -13,11 +14,13 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Mailcow component."""
     hass.data.setdefault(DOMAIN, {})
+    _LOGGER.debug("Mailcow component set up")
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Mailcow from a config entry."""
-    session = async_get_clientsession(hass)
+    _LOGGER.debug(f"Setting up Mailcow entry {entry.entry_id}")
+    session: aiohttp.ClientSession = async_get_clientsession(hass)
     api = MailcowAPI(entry.data, session)
 
     coordinator = MailcowCoordinator(
@@ -34,16 +37,22 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    _LOGGER.info(f"Mailcow entry {entry.entry_id} set up successfully")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+    _LOGGER.debug(f"Unloading Mailcow entry {entry.entry_id}")
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
+        LOGGER.info(f"Mailcow entry {entry.entry_id} unloaded successfully")
+    else:
+        _LOGGER.warning(f"Failed to unload Mailcow entry {entry.entry_id}")
     return unload_ok
 
 async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Handle reload of an entry."""
+    _LOGGER.debug(f"Reloading Mailcow entry {entry.entry_id}")
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
