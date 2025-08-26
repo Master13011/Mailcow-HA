ae0c06a6cfca38070b73f14108dde1d4bbc50d52
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-08-26 16:23:39 +0200

    Update sensor.py

--- custom_components/mailcow_ha_custom/sensor.py
+++ custom_components/mailcow_ha_custom/sensor.py
@@ -2,7 +2,7 @@ import logging
 from urllib.parse import urlparse
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.components.sensor import SensorEntity, SensorStateClass
-from homeassistant.helpers.entity import DeviceEntryType
+from homeassistant.helpers.device_registry import DeviceEntryType
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -115,6 +115,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         MailcowContainersStatusSensor(coordinator),
     ]
     async_add_entities(sensors, True)
+
 
 
 
